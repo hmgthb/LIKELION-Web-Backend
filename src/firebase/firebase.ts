e31efde84ffc7910ac523d78855b29eec9e583c5
Keonsang joh <johkeonsang@gmail.com>
2026-02-13 16:00:54 -0500

    - supabase와 firbase 계정 변경

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH ||
-  path.join(__dirname, '../../llushomepage-firebase-adminsdk-fbsvc-3d16c77e24.json');
+  path.join(__dirname, '../../llnyuhomepage-firebase-adminsdk-fbsvc-d700a5ea66.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountPath),
